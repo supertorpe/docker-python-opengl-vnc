ffedf201c37e75ba42f299675ba71572525c3f06
supertorpe <supertorpe@hotmail.com>
2023-11-13 19:59:07 +0100

    Add jupyter

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.10-slim
 
 RUN apt update && apt -y install freeglut3-dev x11vnc xvfb jwm &&\
-    pip install PyOpenGL &&\
+    pip install PyOpenGL notebook &&\
     mkdir -p /app/code
 COPY startup.sh /app/startup.sh
 RUN chmod -R 777 /app
--- startup.sh
+++ startup.sh
@@ -3,7 +3,8 @@
 # start X11 + Window Manager + VNC server
 
 export DISPLAY=:99
-Xvfb ${DISPLAY} -ac -listen tcp -screen 0 800x600x16 &
+jupyter notebook --notebook-dir=/app/code --ip='*' --port=8888 --no-browser --allow-root &
+Xvfb ${DISPLAY} -ac -listen tcp -screen 0 1024x768x16 &
 sleep 3
 jwm -display ${DISPLAY} &
 sleep 3
